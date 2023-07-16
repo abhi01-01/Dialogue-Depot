e1d778aa3e4b7e0deb5525886259b42a39b91532
Abhishek Singh <abhisingh100392@gmail.com>
2023-07-16 15:00:20 +0530

    Dialogue-Depot-first-commit

---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,57 @@
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+
+function Homepage() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      
+      <Box bg="#dbcdb4"
+           marginTop="30px"
+           marginLeft="30rem"
+           w="90%" 
+           p={4} 
+           borderRadius="lg" 
+           borderWidth="1px"
+           borderColor="crimson">
+
+            
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+      
+    </Container>
+  );
+}
+
+export default Homepage;
